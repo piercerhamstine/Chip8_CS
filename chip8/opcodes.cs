499b1b33ab5185cca0b825a98195fac6dcff7a36
pierce <piercerhamstine@gmail.com>
2024-08-24 19:47:50 -0500

    Added 8k opcodes, fixed instructions checking wrong value

--- chip8/opcodes.cs
+++ chip8/opcodes.cs
@@ -9,10 +9,22 @@ enum OpCodes: uint{
     SEVXNN = 0x3000,
     // Skip next instruction if VX != NN
     SNEVXNN = 0x4000,
-    U = 0x5000,
+    // Skip next instruction if registers VX == VY
+    SEVXVY = 0x5000,
     LDVXNN = 0x6000,
     // Add NN to VX, Store in VX
     ADDVXNN = 0x7000,
+    SETVXVY = 0x8000,
+    SETVXVYOR = 0x8001,
+    SETVXVYAND = 0x8002,
+    SETVXVYXOR = 0x8003,
+    ADDVXVY = 0x8004,
+    SUBVYVX = 0x8005,
+    SHIFTVXRIGHT = 0x8006,
+    SHIFTVXLEFT = 0x800E,
+    // Skip next instruction if VX != VY
+    SNEVXVY = 0x9000,
+    // Sets I to address NNN
     SETI = 0xA000,
     DRAW = 0xD000,
 }
